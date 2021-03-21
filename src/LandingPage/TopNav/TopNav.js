9daf9474760f89ef89328fa55fdc241a6b769fb4
cft012 <tk.suzuki08@gmail.com>
2021-03-21 18:40:39 +0900

    Search Bar

--- src/LandingPage/TopNav/TopNav.js
+++ src/LandingPage/TopNav/TopNav.js
@@ -3,14 +3,14 @@ import styles from './TopNav.module.css';
 
 export function TopNav() {
     return (
-        <div class={styles['top-nav']}>
-            <div class={styles.left}>
+        <div className={styles['top-nav']}>
+            <div className={styles.left}>
                 <span>Write a review</span>
                 <span>Events</span>
             </div>
-            <div class={styles.right}>
+            <div className={styles.right}>
                 <span>login</span>
-                <button class="button">Sign Up</button>
+                <button className="button">Sign Up</button>
             </div>
         </div>
     )
---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './SearchBar.module.css'
+
+
+export function SearchBar(){
+    return(
+        <div className="field has-addons">
+            <div className="control">
+                <button className="button is-info is-medium">Search</button>
+            </div>
+            <div className="control">
+                <input className={`input is-medium ${styles["input-control"]}`} type="text" placeholder="Burgers, Barbars"/>
+            </div>
+            <div className="control">
+                <button className="button is-info is-medium">Near</button>
+            </div>
+            <div className="control">
+                <input className={`input is-medium ${styles["input-control"]}`} type="text" placeholder="Where"/>
+            </div>
+            <div className={`button is-medium ${styles["search-button"]}`}>
+                <span className="icon is-small">
+                    <i className="fas fa-search"></i>
+                </span>
+            </div>
+        </div>
+    )
+}
