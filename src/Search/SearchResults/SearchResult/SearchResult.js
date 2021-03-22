4a95a6aa566036b82c38e621906aa95a7ca151a1
cft012 <tk.suzuki08@gmail.com>
2021-03-22 21:49:00 +0900

    Add Search Results

---
+++ src/Search/SearchResults/SearchResult/SearchResult.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './SearchResult.module.css'
+import { BusinessRating } from '../../BusinessRating/BusinessRating'
+
+export function SearchResult(){
+    return(
+        <div className={ styles['search-result']}>
+            <img src='https://via.placeholder.com/210' alt='Business' className={ styles['business-image']}/>
+            <div className={styles['business-info']}>
+                <h2 className='subtitle'>Burger Place</h2>
+                <BusinessRating/>
+                <p>$$</p>
+                <span class="tag">Burgers</span> <span class="tag">Fast Food</span>
+            </div>
+            <div className="contact-info">
+                <p>+1234234324</p>
+                <p>Street 5</p>
+                <p>12345 Berlin</p>
+            </div>
+        </div>
+    );
+}
